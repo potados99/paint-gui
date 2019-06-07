cfd565cd9a3c42e867169a5b0ba00c8fde4336a9
potados99 <potados99@gmail.com>
2019-06-08 05:15:59 +0900

    Test ready

--- src/input.cpp
+++ src/input.cpp
@@ -3,17 +3,23 @@
 #include <stdio.h>
 #include <fcntl.h>
 #include <unistd.h>
-
 #include <linux/input.h>
 #include <linux/input-event-codes.h>
 #include <errno.h>
+#include <time.h>
 
 
 /**
   * Constructor
   */
 input::input(const char *event_fd_path) {
-	
+	fd_ = open(event_fd_path, O_RDONLY | O_NONBLOCK);
+	if (fd_ == -1) {
+		perror("open failed");
+		exit(1); // TODO: change it to throw
+	}
+
+	correction_ = touch_correction();
 }
 
 /**
@@ -120,6 +126,58 @@ int input::touch_read(int fd, touch_event *event, touch_correction *correction) 
   * Public
   */
 void input::start_loop() {
+	// TODO: replace this test code.
+
+	int 		read = 0;
+	int 		line_num = 0;
+	touch_event 	te;
+	
+	bool 		touched = 0;
+	time_t		last_time = 0;
+
+	while (1) {
+		
+		/**
+		  * Poll successful + fd ready + has event
+		  */
+		
+		read = touch_read(fd, &te, NULL); /* read with fd, to te, no correction. */
+	
+		if (read == 0) {
+			/**
+			  * Successfull read.
+			  */
+
+			if (te.touch_state == STATE_TOUCH_DOWN) {
+				touched = 1;
+			}
+			else if (te.touch_state == STATE_TOUCH_UP) {
+				touched = 0;
+			}
+			
+			if (te.touch_state == STATE_TOUCH_DOWN) printf("\n============================= TOUCH START =============================\n");
+			printf("[%ld] X: %5d,\tY: %5d,\tPressure: %5d,\tState: %d\n", line_num++, te.x, te.y, te.pressure, te.touch_state);
+			if (te.touch_state == STATE_TOUCH_UP) printf("============================= TOUCH FINISH =============================\n\n");
+		}
+		else {
+			if (read == 1) {
+				if (touched == 0) {
+					/**
+					 * Only when touch is finished.
+					 */
+					time_t cur_time = time(NULL);
+					if (last_time < cur_time) {
+						printf("waiting...\n");
+						last_time = cur_time;
+					}
+				}
+			}
+			else {
+				fprintf(stdout, "touch_read failed.\n");
+				exit(1);
+			}
+		}		
+	}
 
 }
 

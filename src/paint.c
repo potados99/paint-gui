7b41f238d1ad0b6d6fd57648d9b3bf1cb67427aa
potados99 <potados99@gmail.com>
2019-06-15 05:56:01 +0900

    New test for line.

--- src/paint.c
+++ src/paint.c
@@ -231,6 +231,9 @@ void paint_test(struct paint *context) {
     struct shape *rect = shape_create(ST_RECT_FILL, 140, 110, 50, 50, COLOR(0, 255, 0));
     shapes_list_add(&context->shapes, rect);
     
+    struct shape *line = shape_create(ST_LINEP, 90, 110, 290, 250, COLOR(0, 255, 0));
+    shapes_list_add(&context->shapes, line);
+    
     struct shape *fdraw = shape_create(ST_FREEP, 0, 0, 0, 0, COLOR(0, 0, 255));
     shape_add_point(fdraw, 100, 100);
     shape_add_point(fdraw, 102, 106);
@@ -269,6 +272,12 @@ void paint_test(struct paint *context) {
         usleep(20000);
         
         _transform_shape_and_redraw(context, rect, 1, 1);
+    }
+    
+    for (int i = 0; i < 100; ++i) {
+        usleep(20000);
+        
+        _transform_shape_and_redraw(context, line, 1, 0);
     }
     
 }

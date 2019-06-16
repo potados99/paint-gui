b8ceab2db3fa42e80cd58cb3d1f96130b54a7e7c
potados99 <potados99@gmail.com>
2019-06-16 20:07:32 +0900

    New elipse test

--- src/display.c
+++ src/display.c
@@ -269,7 +269,78 @@ void disp_draw_oval_fill(int x, int y, int width, int height, unsigned short col
 }
 
 void disp_draw_ovalp(int x0, int y0, int x1, int y1, unsigned short color) {
+    ENSURE_POINTS_ORDERED(x0, y0, x1, y1);
     
+    int rx = (x1 - x0 + 1) / 2;
+    int ry = (y1 - y0 + 1) / 2;
+    int xc = x0 + rx;
+    int yc = y0 + ry;
+    
+    float dx, dy, d1, d2, x, y;
+    x = 0;
+    y = ry;
+    
+    // Initial decision parameter of region 1
+    d1 = (ry * ry)
+    - (rx * rx * ry)
+    + (0.25 * rx * rx);
+    dx = 2 * ry * ry * x;
+    dy = 2 * rx * rx * y;
+    
+    // For region 1
+    while (dx < dy) {
+        
+        // Print points based on 4-way symmetry
+        _modify((x + xc) + ((y + yc) * DP_WIDTH), color);
+        _modify((-x + xc) + ((y + yc) * DP_WIDTH), color);
+        _modify((x + xc) + ((-y + yc) * DP_WIDTH), color);
+        _modify((-x + xc) + ((-y + yc) * DP_WIDTH), color);
+
+        // Checking and updating value of
+        // decision parameter based on algorithm
+        if (d1 < 0) {
+            x++;
+            dx = dx + (2 * ry * ry);
+            d1 = d1 + dx + (ry * ry);
+        }
+        else {
+            x++;
+            y--;
+            dx = dx + (2 * ry * ry);
+            dy = dy - (2 * rx * rx);
+            d1 = d1 + dx - dy + (ry * ry);
+        }
+    }
+    
+    // Decision parameter of region 2
+    d2 = ((ry * ry) * ((x + 0.5) * (x + 0.5)))
+    + ((rx * rx) * ((y - 1) * (y - 1)))
+    - (rx * rx * ry * ry);
+    
+    // Plotting points of region 2
+    while (y >= 0) {
+        
+        // printing points based on 4-way symmetry
+        _modify((x + xc) + ((y + yc) * DP_WIDTH), color);
+        _modify((-x + xc) + ((y + yc) * DP_WIDTH), color);
+        _modify((x + xc) + ((-y + yc) * DP_WIDTH), color);
+        _modify((-x + xc) + ((-y + yc) * DP_WIDTH), color);
+        
+        // Checking and updating parameter
+        // value based on algorithm
+        if (d2 > 0) {
+            y--;
+            dy = dy - (2 * rx * rx);
+            d2 = d2 + (rx * rx) - dy;
+        }
+        else {
+            y--;
+            x++;
+            dx = dx + (2 * ry * ry);
+            dy = dy - (2 * rx * rx);
+            d2 = d2 + dx - dy + (rx * rx);
+        }
+    }
 }
 
 void disp_draw_ovalp_fill(int x0, int y0, int x1, int y1, unsigned short color) {
